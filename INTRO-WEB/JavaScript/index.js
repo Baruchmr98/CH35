cf73095982571c7c8741ad155cf8e97a732e89fb
Baruchmr98 <baruchmr98@gmail.com>
2023-12-08 20:38:35 -0600

    CSS-avanzasdo, bootstrap y JS

---
+++ INTRO-WEB/JavaScript/index.js
@@ -0,0 +1,22 @@
+//comentario lineal
+/*comentario multilinea*/
+
+//alert("Hola mundo");
+
+// console.log("Este es un mensaje para mi consola");
+
+/*var nombre = prompt("Ingresa tu nombre, porfavor.");
+alert("Hola, " + nombre + ". Bienvenido a la ch35.");*/
+
+// var numero1 = parseInt(prompt("Ingresa el primer número: "));
+// var numero2 = parseInt(prompt("Ingresa el segundo número: "));
+
+// console.log("Suma: " + (numero1 + numero2))
+// console.log("Resta: " + (numero1 - numero2))
+// console.log("Multiplicación: " + (numero1 * numero2))
+// console.log("División: " + (numero1 / numero2))
+
+
+// var edad = parseInt(prompt("Ingresa tu edad, va?: ")); 
+// alert("¿" + (edad>18) + "?" + " Uy no, muchache. Regrésate.");
+
